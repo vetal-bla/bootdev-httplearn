c7a76826a4d55020bbda8b3e22098b90e6436448
vitalii <it-finally@pm.me>
2026-03-11 16:20:16 +0100

    added healthz endpoint

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	serverPort := ":8080"
 
 	servMux := http.NewServeMux()
-	servMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	servMux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	servMux.HandleFunc("/healthz", handlerHealthz)
 	srv := http.Server{
 		Handler: servMux,
 		Addr:    serverPort,
@@ -18,4 +19,11 @@ func main() {
 	fmt.Printf("Start server on port: %s\nWhich server dir: %s\n", serverPort, filepathRoot)
 	srv.ListenAndServe()
 
+}
+
+func handlerHealthz(res http.ResponseWriter, req *http.Request) {
+	// fmt.Println("test - handler")
+	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("OK"))
 }
