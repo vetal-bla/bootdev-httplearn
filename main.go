85aa96fae8081f3fedd254bb90ab3b234b36d2b7
vitalii <it-finally@pm.me>
2026-03-12 08:51:01 +0100

    forget to save main go

--- main.go
+++ main.go
@@ -36,9 +36,3 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 
 }
-
-func (c *apiconfig) handlerReset(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	c.fileServerHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-}
